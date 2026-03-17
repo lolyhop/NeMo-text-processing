93a5785b2580dbe257a9827d1d8fdbcc737cd8bc
Egor Chernobrovkin <33jasooon33@gmail.com>
2026-03-17 19:25:06 +0300

    fix repeated numbers

--- nemo_text_processing/inverse_text_normalization/ru/taggers/cardinal.py
+++ nemo_text_processing/inverse_text_normalization/ru/taggers/cardinal.py
@@ -36,13 +36,45 @@ class CardinalFst(GraphFst):
         graph = tn_cardinal.cardinal_numbers_default
         self.graph = graph.invert().optimize()
 
+        repeat_count_words = {
+            2: ["два", "две"],
+            3: ["три"],
+            4: ["четыре"],
+            5: ["пять"],
+            6: ["шесть"],
+            7: ["семь"],
+            8: ["восемь"],
+            9: ["девять"],
+        }
+        repeated_digit_nouns = {
+            "0": ["ноль", "нуля", "нулей"],
+            "1": ["единица", "единицы", "единиц"],
+            "2": ["двойка", "двойки", "двоек"],
+            "3": ["тройка", "тройки", "троек"],
+            "4": ["четверка", "четверки", "четверок"],
+            "5": ["пятерка", "пятерки", "пятерок"],
+            "6": ["шестерка", "шестерки", "шестерок"],
+            "7": ["семерка", "семерки", "семерок"],
+            "8": ["восьмерка", "восьмерки", "восьмерок"],
+            "9": ["девятка", "девятки", "девяток"],
+        }
+        repeated_digits = pynini.string_map(
+            [
+                (f"{count_word} {digit_noun}", digit * repeat_count)
+                for repeat_count, count_words in repeat_count_words.items()
+                for count_word in count_words
+                for digit, digit_nouns in repeated_digit_nouns.items()
+                for digit_noun in digit_nouns
+            ]
+        )
+
         optional_sign = pynini.closure(
             pynutil.insert("negative: ") + pynini.cross("минус ", "\"-\"") + insert_space, 0, 1
         )
 
         # do not invert numbers less than 10
         # @chrnegor: Allow them too
-        graph = pynini.compose(graph, NEMO_DIGIT ** (1, ...))
+        graph = pynini.compose(graph, NEMO_DIGIT ** (1, ...)) | repeated_digits
         graph = optional_sign + pynutil.insert("integer: \"") + graph + pynutil.insert("\"")
         graph = self.add_tokens(graph)
         self.fst = graph.optimize()
